87c909939536e0853746162197368e77c5d1da10
weixiaowei <1575144082@qq.com>
2014-08-08 14:26:56 +0800

    ps1.3

---
+++ alphabetical substring.py
@@ -0,0 +1,11 @@
+s = raw_input('Enter a string: ')
+curString = s[0]
+longest = s[0]
+for i in range(1, len(s)):
+    if s[i] >= curString[-1]:
+        curString += s[i]
+        if len(curString) > len(longest):
+            longest = curString
+    else:
+        curString = s[i]
+print 'longest subString in alphabetical order is: ', longest
