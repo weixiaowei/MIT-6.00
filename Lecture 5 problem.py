bde0190fa451b66605c7e0bce747df24e1413390
weixiaowei <1575144082@qq.com>
2014-08-10 16:30:57 +0800

    Update Lecture 5 problem.py

--- Lecture 5 problem.py
+++ Lecture 5 problem.py
@@ -172,3 +172,40 @@ def isIn(char, aStr):
    #  so recursively search on the last half of aStr
    else:
       return isIn(char, aStr[midIndex+1:])
+
+
+
+#L5 problem 9
+#wrapper function calls
+def semordnilapWrapper(str1, str2):
+    # A single-length string cannot be semordnilap
+    if len(str1) == 1 or len(str2) == 1:
+        return False
+
+    # Equal strings cannot be semordnilap
+    if str1 == str2:
+        return False
+
+    return semordnilap(str1, str2)
+    
+    
+def semordnilap(str1, str2):
+    '''
+    str1: a string
+    str2: a string
+    
+    returns: True if str1 and str2 are semordnilap;
+             False otherwise.
+    '''
+    # If strings aren't the same length, they cannot be semordnilap
+    if len(str1) != len(str2):
+        return False
+
+    # Base case: if the strings are each of length 1, check if they're equal
+    if len(str1) == 1 and len(str2) == 1:
+        return str1 == str2
+
+    # Recursive case: check if the first letter of str1 equals the last letter
+    # of str2
+    else:
+        return str1[0] == str2[-1] and return semordnilap(str1[1:], str2[:-1])
