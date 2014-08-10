8748f5f8497462b5870ffa45e07706633bd416ba
weixiaowei <1575144082@qq.com>
2014-08-10 17:01:00 +0800

    Update Lecture 5 problem.py

--- Lecture 5 problem.py
+++ Lecture 5 problem.py
@@ -209,3 +209,23 @@ def semordnilap(str1, str2):
     # of str2
     else:
         return str1[0] == str2[-1] and return semordnilap(str1[1:], str2[:-1])
+
+
+
+# Globe variables
+def fibMetered(x):
+    global numCalls
+    numCalls += 1
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return fibMetered(x-1) + fibMetered(x-2)
+
+
+def testFib(n):
+    for i in range(n+1):
+        global numCalls
+        numCalls = 0
+        print('fib of ' + str(i) + ' = ' + str(fibMetered(i)))
+        print('fib called ' + str(numCalls) + ' times')
+
