346d465303f51201f2f2bc62a8302155112f36b0
weixiaowei <1575144082@qq.com>
2014-08-10 09:37:00 +0800

    Update Lecture 5 problem.py

--- Lecture 5 problem.py
+++ Lecture 5 problem.py
@@ -72,3 +72,17 @@ def gcdRecur(a, b):
         return a
     else:
         return gcdRecur(b, a % b)
+
+
+
+#Towers of Hanoi
+def printMove(fr, to):
+    print('move from ' + str(fr) + ' to ' + str(to))
+
+def Towers(n, fr, to, spare):
+    if n == 1:
+        printMove(fr, to)
+    else:
+        Towers(n-1, fr, spare, to)
+        Towers(1, fr, to, spare)
+        Towers(n-1, spare, to, fr)
